8fa3e0b62eb2c9baee30ec0a81928ef810659db4
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-03-01 00:19:48 +0630

    Live Check Availability in database Laravel using JQuery Ajax

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -43,4 +43,15 @@ class AuthController extends Controller
 
         return redirect('/')->with('success', 'Register successfully');
     }
+
+    public function CheckEmail(Request $request)
+    {
+        $email = $request->input('email');
+        $isExists = User::where('email', $email)->first();
+        if ($isExists) {
+            return response()->json(array("exists" => true));
+        } else {
+            return response()->json(array("exists" => false));
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -23,3 +14,4 @@ Route::get('/', [AuthController::class, 'index']);
 Route::get('forgot-password', [AuthController::class, 'forgot_password']);
 Route::get('register', [AuthController::class, 'register']);
 Route::post('register_post', [AuthController::class, 'register_post']);
+Route::post('checkemail', [AuthController::class, 'CheckEmail']);
