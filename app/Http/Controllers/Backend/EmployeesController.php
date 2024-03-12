d67d6163f5cb4a509d93383bf91f29d8d46d6711
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-03-12 12:05:55 +0630

    HR Employee new registration email sends employee login detail

--- app/Http/Controllers/Backend/EmployeesController.php
+++ app/Http/Controllers/Backend/EmployeesController.php
@@ -10,6 +10,9 @@ use App\Models\Manager;
 use App\Models\Department;
 use Illuminate\Support\Str;
 use App\Models\Position;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmployeesNewCreateMail;
+use Illuminate\Support\Facades\Hash;
 
 class EmployeesController extends Controller
 {
@@ -51,7 +54,7 @@ class EmployeesController extends Controller
         $user->last_name = trim($request->last_name);
         $user->email = trim($request->email);
         $user->interview = trim($request->interview);
-        $user->password = \Illuminate\Support\Facades\Hash::make($request->password);
+
         $user->phone_number = trim($request->phone_number);
         $user->hire_date = trim($request->hire_date);
         $user->job_id = trim($request->job_id);
@@ -61,6 +64,8 @@ class EmployeesController extends Controller
         $user->department_id  = trim($request->department_id);
         $user->position_id = trim($request->position_id);
         $user->is_role = 0; //0=employee
+        $random_password = $request->password;
+        $user->password = Hash::make($random_password);
 
         if (!empty($request->file('profile_image'))) {
             $file = $request->file("profile_image");
@@ -70,6 +75,8 @@ class EmployeesController extends Controller
             $user->profile_image = $filename;
         }
         $user->save();
+        $user->random_password = $random_password;
+        Mail::to($user->email)->send(new EmployeesNewCreateMail($user));
 
         return redirect('admin/employees')->with('success', 'Employees successfully register.');
     }
@@ -122,7 +129,7 @@ class EmployeesController extends Controller
         $user->is_role = 0; //0=employee
 
         if (!empty($request->password)) {
-            $user->password = \Illuminate\Support\Facades\Hash::make($request->password);
+            $user->password = Hash::make($request->password);
         }
 
         if (!empty($request->file('profile_image'))) {
